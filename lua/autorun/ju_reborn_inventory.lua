41318d4428e1e7007dd605d60b1c6f8a103fbc78
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-16 16:29:59 +0300

    Добавил самую базу

---
+++ lua/autorun/ju_reborn_inventory.lua
@@ -0,0 +1,54 @@
+
+-- by jujaken
+
+ju = ju or {}
+ju.reborn_inventory = ju.reborn_inventory or {}
+ju.reborn_inventory.cfg = ju.reborn_inventory.cfg or {}
+ju.reborn_inventory.db = ju.reborn_inventory.db or {}
+
+-- include functions:
+
+local rDir = 'ju_reborn_inventory/'
+
+local addSv = SERVER and include or function() end
+local addCl = SERVER and AddCSLuaFile or include
+local addSh = function(...)
+    addSv(...)
+    addCl(...)
+end
+
+local addFile = function(name, dir)
+    local prefix = string.Left(name, 3)
+    local path = (dir or rDir) .. name
+
+    if prefix == 'sv_' then
+        addSv(path)
+        return
+    end
+
+    if prefix == 'cl_' then
+        addCl(path)
+        return
+    end
+
+    addSh(path)
+end
+
+local addDir = function(name)
+    local dir = rDir..name..'/'
+    local files, _ = file.Find(dir..'*', 'LUA')
+    
+    for _, v in ipairs(files) do
+        if !string.EndsWith(v, '.lua') then return end
+        addFile(v, dir)
+    end
+end
+
+addFile 'cfg.lua'
+addFile 'db.lua'
+
+addDir 'core'
+addDir 'derma'
+
+-- Hello msg:
+MsgC(Color(134, 156, 223), "\n~~~\tJuliandrKen (jujaken) Reborn Inventory was loaded. I'm hokaga!\t~~~\n")
