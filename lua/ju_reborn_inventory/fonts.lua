9cc95f19dccc67839c375b444394338fabe25447
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-17 16:05:37 +0300

    Усилил шрифты

--- lua/ju_reborn_inventory/fonts.lua
+++ lua/ju_reborn_inventory/fonts.lua
@@ -5,12 +5,12 @@ surface.CreateFont('juRebornInventoryTitle1', {
     font = 'Montserrat Medium',
     size = 36,
     extended = true,
-    weight = 600,
+    weight = 600 * 1000,
 })
 
 surface.CreateFont('juRebornInventoryTitle2', {
     font = 'Montserrat Bold',
     size = 24,
     extended = true,
-    weight = 800,
+    weight = 800 * 1000,
 })
