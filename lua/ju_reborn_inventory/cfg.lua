041c46740f27bfb3ecac4c15458ef3d636bc78e1
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2023-07-17 10:40:45 +0300

    Конфиги для ячейки

---
+++ lua/ju_reborn_inventory/cfg.lua
@@ -0,0 +1,19 @@
+local cfg = ju.reborn_inventory.cfg
+
+if SERVER then
+    
+    
+
+else
+
+    -- c-menu inv sizes:
+    cfg.inventory_cell_c_size = 140
+    cfg.inventory_cell_c_strokeSize = 1
+
+    cfg.inventory_cell_stripes_interval = 3
+
+    -- c-menu inv colors:
+    cfg.inventory_cell_c_main_color = fromHex '#88EECC'
+    cfg.inventory_cell_num_stripes = 10
+
+end
